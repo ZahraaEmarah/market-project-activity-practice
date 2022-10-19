22cb9170376d1b557bbf38985a74c662243c6eca
ZahraaEmarah <zahraa.emara@syngenta.com>
2022-10-19 19:36:00 +0200

    updated product module to validate required fields

--- src/Models/product.js
+++ src/Models/product.js
@@ -2,11 +2,14 @@ var mongoose = require('mongoose');
 
 
 const productSchema = new mongoose.Schema({
-    id: String,
-    name: String,
-    price: Number,
-    quantity: Number,
-    category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' }
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    description: String,
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+    img_url: String,
+    images: [String]
 });
 
 module.exports = mongoose.model('Product', productSchema);
