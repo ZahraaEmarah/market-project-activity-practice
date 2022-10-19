77711aaa39a554e31848068feeeaa924c366dae1
ZahraaEmarah <zahraa.emara@syngenta.com>
2022-10-19 19:36:53 +0200

    added products endpoints

--- src/Routers/productsRoutes.js
+++ src/Routers/productsRoutes.js
@@ -3,14 +3,94 @@ const router = express.Router()
 const Product = require("../Models/product")
 
 // fetch all products
-router.get("/", async (req, res) => {
-    const products = await Product.find()
-    res.send({
-        "success": true,
-        "results": products,
-        "messages": "OK"
-    })
-})
+router.route("/")
+    .get((req, res) => {
+        var query = {}
+        if (req.query.categoryID) {
+            query = { "category_id": req.query.categoryID }
+            console.log(query)
+        }
+        Product.find(query, (err, products) => {
+            if (err) {
+                res.status(500).json({
+                    "success": false,
+                    "results": [],
+                    "messages": err
+                })
+            }
+            res.json({
+                "success": true,
+                "results": products,
+                "messages": "OK"
+            })
+        })
 
+    })
+    .post(async (req, res) => {
+        if (Object.keys(req.body).length === 0) {
+            res.status(400).json({
+                "success": false,
+                "messages": "Missing request body"
+            })
+        }
+
+        const product = new Product(req.body)
+        await product.save((err) => {
+            if (err) {
+                res.status(500).json({
+                    "success": false,
+                    "results": [],
+                    "messages": err
+                })
+            }
+            res.status(201).json({
+                "success": true,
+                "messages": "Product created successfully"
+            })
+        })
+    })
+
+
+router.route("/:id")
+    .get(async (req, res) => {
+        Product.findById(req.params.id, (err, product) => {
+            if (err) {
+                res.status(500).json({
+                    "success": false,
+                    "result": {},
+                    "messages": err
+                })
+            }
+            res.json({
+                "success": true,
+                "result": product,
+                "messages": "OK"
+            })
+        })
+    })
+    .put((req, res) => {
+        const newProduct = req.body
+        
+        if (Object.keys(req.body).length === 0) {
+            res.status(400).json({
+                "success": false,
+                "messages": "Missing request body"
+            })
+        }
+
+        Product.findByIdAndUpdate(req.params.id, newProduct, (err, product) => {
+            if (err) {
+                res.status(500).json({
+                    "success": false,
+                    "result": {},
+                    "messages": err
+                })
+            }
+            res.json({
+                "success": true,
+                "messages": "Product updated successfully"
+            })
+        })
+    })
 
 module.exports = router
